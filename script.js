c59055035cddb252d479865171bf56afea51732c
Skalex Stuch <skalexstuch@me.com>
2022-09-27 19:00:53 +0800

    updated clase

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+//document.blabla es una de las funciones de js!
+
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
-const clase = document.querySelector('.clase');
+const clase = document.querySelector('.test');
 const parrafito = document.querySelector('#id');
+const input = document.querySelector('input');
 
-//console.log(input.value);
+console.log(input.value);
 
 console.log({
     h1,
     p,
     clase,
-    parrafito
-})
+    parrafito,
+    input
+    });
