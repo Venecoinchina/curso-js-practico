5bbc552a5c46b677283ff6aa59728789d8f5b7b8
Skalex Stuch <skalexstuch@me.com>
2022-09-27 18:48:24 +0800

    upated js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const clase = document.querySelector('.clase');
+const parrafito = document.querySelector('#id');
+
+//console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    clase,
+    parrafito
+})
